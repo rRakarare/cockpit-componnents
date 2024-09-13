bab139553742964c96e1544ceef478418c05d1f8
rRakarare <rRene333@googlemail.com>
2024-09-13 12:00:21 +0200

    asdqwe123

--- src/pages/chat/page.jsx
+++ src/pages/chat/page.jsx
@@ -1,10 +1,12 @@
 import ChatInput from "@/components/custom/chat/chat-input";
+import CockpitLogo from "@/layout/_components/logo";
 
 function Page() {
   return (
     <div className="flex flex-col h-screen">
       <div className="mt-topbar pb-4 size-full overflow-y-auto">
-        <div className="container flex h-full items-center justify-center">
+        <div className="container flex flex-col h-full items-center justify-center">
+          <CockpitLogo className="w-[500px] absolute fill-accent -z-30" />
         <h2 className="text-4xl font-bold">Was geht ab heute?</h2>
         </div>
       </div>
