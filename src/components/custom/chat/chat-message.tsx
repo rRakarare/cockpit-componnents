0d5db2fcfc29fb6d2fcefc1272aa23fdcdc39722
rRakarare <rRene333@googlemail.com>
2024-09-17 21:16:34 +0200

    chatIcons

--- src/components/custom/chat/chat-message.tsx
+++ src/components/custom/chat/chat-message.tsx
@@ -1,10 +1,18 @@
 import { MessageType } from "@/config/chat/base-chat/types";
 import Markdown from "../markdown/markdown";
+import CockpitLogo from "@/components/layout/_components/logo";
+import { User } from "lucide-react";
 
-function ChatMessage({ content }: Partial<MessageType>) {
+function ChatMessage({ content, role }: Partial<MessageType>) {
   return (
-    <div className="container max-w-3xl">
-      <div className="p-6 rounded-lg my-2 border">
+    <div className="container max-w-3xl flex my-2 space-x-4">
+      {role === "assistant" ? (
+        <CockpitLogo className="size-7 mt-5 p-1 shrink-0 border rounded-md shadow-sm" />
+      ) : (
+        <User className="size-7 mt-5 p-1 shrink-0 border rounded-md shadow-sm" />
+      )}
+
+      <div className="py-4 w-full border-b">
         <Markdown>{content}</Markdown>
       </div>
     </div>
