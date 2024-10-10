6744be5520af18ff280f7f00c99796e76e09f9d8
rRakarare <rRene333@googlemail.com>
2024-10-10 13:23:11 +0200

    navAdj

--- src/components-v2/layout/navigation/top-part.tsx
+++ src/components-v2/layout/navigation/top-part.tsx
@@ -1,10 +1,4 @@
 import CockpitLogo from "@/components-v2/custom/unsorted/logo";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components-v2/ui/tooltip";
 import { cn } from "@/lib-v2/utils";
 import { Sidebar } from "lucide-react";
 import { Link } from "react-router-dom";
@@ -23,27 +17,9 @@ function TopPart({ setIsOpen, isOpen }: TopPartProps) {
       <Link to={"/"} className="">
         <CockpitLogo className="size-10" />
       </Link>
-      <TooltipProvider>
-        <Tooltip delayDuration={0}>
-          <TooltipTrigger asChild>
-
-              <div className={cn("hover:bg-accent p-2 rounded-lg border-2 border-transparent cursor-pointer", !isOpen && "opacity-0")} onClick={() => setIsOpen((state) => !state)}>
+      <div className={cn("hover:bg-accent p-2 rounded-lg border-2 border-transparent cursor-pointer", !isOpen && "hidden")} onClick={() => setIsOpen((state) => !state)}>
               <Sidebar className="size-6 shrink-0"  />
               </div>
-
-   
-          </TooltipTrigger>
-          <TooltipContent
-            side="right"
-            sideOffset={18}
-            align="start"
-            alignOffset={2}
-            className="bg-foreground text-background"
-          >
-            <p>Toggle Sidebar</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
     </div>
   );
 }
