53f621f86df54b2fad042f41df93a993aa2b8e7e
rRakarare <rRene333@googlemail.com>
2024-10-17 18:04:27 +0200

    newLint

--- src/pages/data-chat/id/page.tsx
+++ src/pages/data-chat/id/page.tsx
@@ -1,18 +1,15 @@
-import { useParams } from "react-router-dom";
-import { useChat } from "../_components/hooks/useChat";
 import ChatMessageList from "@/components-v2/custom/chat/chat-message-list";
 import ChatSettings from "../_components/ChatSettings";
+import { useChat } from "@/pages/chat/_components/hooks/useChat";
 
 function Page() {
 
-  const { id } = useParams<{ id: string }>();
-  
-  const { chat, isLoadingChats, stream, upstream, response } = useChat(id);
+  const { chat, isLoadingChats, stream, upstream, response, isFinished,isStreaming } = useChat();
 
 
   return (
     <div className="flex flex-col h-screen max-h-screen justify-center">
-      <ChatMessageList messages={chat?.messages} isLoading={isLoadingChats} upstream={upstream} response={response} />
+      <ChatMessageList messages={chat?.messages} isLoading={isLoadingChats} upstream={upstream} response={response} isFinished={isFinished} isStreaming={isStreaming} />
       <ChatSettings onSend={stream} />
     </div>
   );
