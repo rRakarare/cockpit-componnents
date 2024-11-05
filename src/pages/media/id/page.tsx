f8d5f64c4aabf5b981a0b4fb6619d6e8bb851693
rRakarare <rRene333@googlemail.com>
2024-11-05 19:09:49 +0100

    nbewImage

--- src/pages/media/id/page.tsx
+++ src/pages/media/id/page.tsx
@@ -1,21 +1,19 @@
-import { imageChats } from "@/config-v2/chat/image-chat/example-data";
-import { useParams } from "react-router-dom";
 
 function Page() {
 
-  const {id} = useParams<{id: string}>();
+  // const {id} = useParams<{id: string}>();
 
-  const imageChat = imageChats.find((chat) => chat._id === id);
+  // const imageChat = imageChats.find((chat) => chat._id === id);
 
   return (
     <div className="flex flex-col h-screen">
       <div className="my-topbar pb-4 size-full overflow-y-auto flex items-center justify-center flex-col space-y-4">
         <div className="border p-4 rounded-xl shadow-sm">
-          {imageChat?.messages[0].content}
+        Create a surreal landscape with floating islands and a purple sky
         </div>
         <div className="flex">
-        <img className="object-fit p-4 size-96 shadow-sm m-4 hover:scale-110 transition-all border" src={imageChat?.messages[1].content}/>
-        <img className="object-fit p-4 size-96 shadow-sm m-4 hover:scale-110 transition-all border" src={imageChat?.messages[1].contentTwo}/>
+        <img className="object-fit p-4 size-96 shadow-sm m-4 hover:scale-110 transition-all border" src={"https://picsum.photos/700/700"}/>
+        <img className="object-fit p-4 size-96 shadow-sm m-4 hover:scale-110 transition-all border" src={"https://picsum.photos/700/700"}/>
         </div>
       </div>
     </div>
